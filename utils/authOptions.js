c0f0612844236374305e683baf100e3b5b7c19da
Marc Granier <mgranier@comcast.net>
2024-04-02 16:54:34 -0400

    Completed profile image

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,5 +1,7 @@
+import connectDB from '@/config/database';
+import User from '@/models/User';
+
 import GoogleProvider from 'next-auth/providers/google';
-import { FaDatabase, FaExpeditedssl } from 'react-icons/fa';
 
 export const authOptions = {
 	providers: [
@@ -16,18 +18,34 @@ export const authOptions = {
 		}),
 	],
 	callbacks: {
-		//invoke on successful signin
+		// Invoked on successful signin
 		async signIn({ profile }) {
 			// 1. Connect to database
+			await connectDB();
 			// 2. Check if user exists
+			const userExists = await User.findOne({ email: profile.email });
 			// 3. If not, then add user to database
-			// 4. Return true to allow signIn
+			if (!userExists) {
+				// Truncate user name if too long
+				const username = profile.name.slice(0, 20);
+
+				await User.create({
+					email: profile.email,
+					username,
+					image: profile.picture,
+				});
+			}
+			// 4. Return true to allow sign in
+			return true;
 		},
-		//Modifies the session object
-		async session() {
+		// Modifies the session object
+		async session({ session }) {
 			// 1. Get user from database
+			const user = await User.findOne({ email: session.user.email });
 			// 2. Assign the user id to the session
-			// 3. Return session
+			session.user.id = user._id.toString();
+			// 3. return session
+			return session;
 		},
 	},
 };
