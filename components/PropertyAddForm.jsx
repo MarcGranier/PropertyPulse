cfa31e4af93b5584651f3597538a00b03e98e9f1
Marc Granier <mgranier@comcast.net>
2024-04-03 14:58:38 -0400

    Complete Add property POST API route \

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -102,7 +102,11 @@ const PropertyAddForm = () => {
 
 	return (
 		mounted && (
-			<form>
+			<form
+				action='/api/properties'
+				method='POST'
+				encType='multipart/form-data'
+			>
 				<h2 className='text-3xl text-center font-semibold mb-6'>
 					Add Property
 				</h2>
@@ -217,7 +221,7 @@ const PropertyAddForm = () => {
 							name='beds'
 							className='border rounded w-full py-2 px-3'
 							required
-							value={fields.bed}
+							value={fields.beds}
 							onChange={handleChange}
 						/>
 					</div>
@@ -508,7 +512,7 @@ const PropertyAddForm = () => {
 					<input
 						type='text'
 						id='seller_name'
-						name='seller_info.name.'
+						name='seller_info.name'
 						className='border rounded w-full py-2 px-3'
 						placeholder='Name'
 						value={fields.seller_info.name}
@@ -566,6 +570,7 @@ const PropertyAddForm = () => {
 						accept='image/*'
 						multiple
 						onChange={handleImageChange}
+						required
 					/>
 				</div>
 
