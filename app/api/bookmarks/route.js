b1526c0daafac502162f55a2c5afa0240059040f
Marc Granier <mgranier@comcast.net>
2024-04-05 14:40:26 -0400

    Complete Bookmark Button Request

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -3,7 +3,33 @@ import User from '@/models/User';
 import Property from '@/models/Property';
 import { getSessionUser } from '@/utils/getSessionUser';
 
-export const dynamic = 'force-dymamic';
+export const dynamic = 'force-dynamic';
+
+// GET /api/bookmarks
+export const GET = async () => {
+	try {
+		await connectDB();
+
+		const sessionUser = await getSessionUser();
+
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response('User ID is required', { status: 401 });
+		}
+
+		const { userId } = sessionUser;
+
+		// Find user in database
+		const user = await User.findOne({ _id: userId });
+
+		// Get users bookmarks
+		const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+		return new Response(JSON.stringify(bookmarks), { status: 200 });
+	} catch (error) {
+		console.log(error);
+		return new Response('Something went wrong', { status: 500 });
+	}
+};
 
 export const POST = async (request) => {
 	try {
@@ -13,16 +39,16 @@ export const POST = async (request) => {
 
 		const sessionUser = await getSessionUser();
 
-		if (!session || session.userId) {
-			return new Response('User Id is required', { status: 401 });
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response('User ID is required', { status: 401 });
 		}
 
 		const { userId } = sessionUser;
 
-		//Find userin database
+		// Find user in database
 		const user = await User.findOne({ _id: userId });
 
-		//Check if property is bookmarked
+		// Check if property is bookmarked
 		let isBookmarked = user.bookmarks.includes(propertyId);
 
 		let message;
@@ -33,13 +59,14 @@ export const POST = async (request) => {
 			message = 'Bookmark removed successfully';
 			isBookmarked = false;
 		} else {
-			//If not bookmarked
+			// If not bookmarked, add it
 			user.bookmarks.push(propertyId);
 			message = 'Bookmark added successfully';
 			isBookmarked = true;
 		}
 
 		await user.save();
+
 		return new Response(JSON.stringify({ message, isBookmarked }), {
 			status: 200,
 		});
