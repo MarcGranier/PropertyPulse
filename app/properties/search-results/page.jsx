cbb6c0502ebdddce5c30961cde37c5e571f0e47a
Marc Granier <mgranier@comcast.net>
2024-04-07 16:31:27 -0400

    Working on Search

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,12 +1,39 @@
 'use client';
-
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 const SearchResultsPage = () => {
 	const searchParams = useSearchParams();
 
-	console.log(searchParams.get('location'));
+	const [properties, setProperties] = useState([]);
+	const [loading, setLoading] = useState([]);
+
+	const location = searchParams.get('location');
+	const propertyType = searchParams.get('propertyType');
+
+	useEffect(() => {
+		const fetchSearchResults = async () => {
+			try {
+				const res = await fetch(
+					`/api/properties/search?=${location}&propertyType=${propertyType}`
+				);
+
+				if (res.status === 200) {
+					const data = await res.json();
+					setProperties(data);
+				} else {
+					setProperties([]);
+				}
+			} catch (error) {
+				console.log(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchSearchResults();
+	}, [location, propertyType]);
+
+	console.log(properties);
 
 	return <div>SearchResultsPage</div>;
 };
