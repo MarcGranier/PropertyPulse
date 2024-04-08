2ca80566c8cb46516f9b55c31643638d1ed355ea
Marc Granier <mgranier@comcast.net>
2024-04-08 14:30:33 -0400

    Complete Message Submit & API Route

--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -1,8 +1,11 @@
 'use client';
+import { useSession } from 'next-auth/react';
 import { useState } from 'react';
 import { FaPaperPlane } from 'react-icons/fa';
+import { toast } from 'react-toastify';
 
 const PropertyContactForm = ({ property }) => {
+	const { data: session } = useSession();
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 	const [message, setMessage] = useState('');
@@ -21,17 +24,43 @@ const PropertyContactForm = ({ property }) => {
 			property: property._id,
 		};
 
-		console.log(data);
+		try {
+			const res = await fetch('/api/messages', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			});
 
-		setWasSubmitted(true);
+			if (res.status === 200) {
+				toast.success('Message sent successfully');
+				setWasSubmitted(true);
+			} else if (res.status === 400 || res.status === 401) {
+				const dataObj = await res.json();
+				toast.error(dataObj.message);
+			} else {
+				toast.error('Error sending form');
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error('Error sending form');
+		} finally {
+			setName('');
+			setEmail('');
+			setPhone('');
+			setMessage('');
+		}
 	};
 
 	return (
 		<div className='bg-white p-6 rounded-lg shadow-md'>
 			<h3 className='text-xl font-bold mb-6'>Contact Property Manager</h3>
-			{wasSubmitted ? (
-				<p className='text-green-600 mb-4'>
-					Your message has been submitted successfully
+			{!session ? (
+				<p>You must be logged in to send a message</p>
+			) : wasSubmitted ? (
+				<p className='text-green-500 mb-4'>
+					Your message has been sent successfully
 				</p>
 			) : (
 				<form onSubmit={handleSubmit}>
@@ -105,8 +134,7 @@ const PropertyContactForm = ({ property }) => {
 							className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center'
 							type='submit'
 						>
-							<FaPaperPlane className='mr-2' />
-							Send Message
+							<FaPaperPlane className='mr-2' /> Send Message
 						</button>
 					</div>
 				</form>
